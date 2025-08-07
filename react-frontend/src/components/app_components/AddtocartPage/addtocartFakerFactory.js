d58e152915458bf7b2c4272de469e0230564f92f
Code Bridge Machine <ahmed@codebridge.my>
2025-08-07 10:35:05 +0800

    CodeBridge commit

---
+++ react-frontend/src/components/app_components/AddtocartPage/addtocartFakerFactory.js
@@ -0,0 +1,18 @@
+
+import { faker } from "@faker-js/faker";
+export default (user,count) => {
+    let data = [];
+    for (let i = 0; i < count; i++) {
+        const fake = {
+product: faker.datatype.number(""),
+quantity: faker.datatype.number(""),
+payment: faker.lorem.sentence(""),
+total: faker.datatype.number(""),
+
+updatedBy: user._id,
+createdBy: user._id
+        };
+        data = [...data, fake];
+    }
+    return data;
+};
---
+++ react-frontend/src/components/app_components/ProductPage/productFakerFactory.js
@@ -0,0 +1,18 @@
+
+import { faker } from "@faker-js/faker";
+export default (user,count) => {
+    let data = [];
+    for (let i = 0; i < count; i++) {
+        const fake = {
+name: faker.datatype.number(""),
+description: faker.datatype.number(""),
+quantity: faker.datatype.number(""),
+category: faker.lorem.sentence(""),
+
+updatedBy: user._id,
+createdBy: user._id
+        };
+        data = [...data, fake];
+    }
+    return data;
+};
